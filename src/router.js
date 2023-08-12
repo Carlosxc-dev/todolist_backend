403a1cfcdb2884b51e06e76a4efbb8fe3930ba07
Carlos Henrique <chcruz64@gmail.com>
2023-08-12 10:53:40 -0300

    up

---
+++ src/router.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const taskController = require('./controller/taskController')
+const middleware = require('./middlewares/middlewaresTask')
+
+const router = express.Router()
+
+router.get('/task', taskController.getAll);
+router.post('/task', middleware.validateFieldTitle, taskController.createTask);
+router.delete('/task/:id', taskController.deleteTask);
+router.put('/task/:id',
+    middleware.validateFieldTitle,
+    middleware.validateFieldStatus,
+    taskController.updateTask,
+);
+
+module.exports = router
